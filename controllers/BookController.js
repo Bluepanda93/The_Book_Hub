13c4321f5607dea049d7aeaec6fecd8e51bbe138
Daniel Shull <danielshull121@gmail.com>
2022-11-14 11:31:12 -0500

    create route

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -12,6 +12,29 @@ const GetBooks = async (req, res) => {
     }
 }
 
+const GetBookById = async (req, res) => {
+    try {
+        const bookById = await Books.findByPk(req.params.book_id)
+        res.send(bookById)
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+const CreateBook = async (req, res) => {
+    console.log(req.body)
+    try {
+        const post = await Books.create({ ...req.body })
+        res.send(post)
+    } catch (error) {
+        throw error
+    }
+}
+
+
 module.exports = {
-    GetBooks
+    GetBooks,
+    GetBookById,
+    CreateBook
 }
