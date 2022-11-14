ead05378bb9ff9b9c10cde5dcfbf74583e056451
Daniel Shull <danielshull121@gmail.com>
2022-11-14 12:01:11 -0500

    back-end CRUD done for books

--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -6,4 +6,5 @@ Router.get('/all_books', controller.GetBooks)
 Router.get('/:book_id', controller.GetBookById)
 Router.post('/created_books', controller.CreateBook)
 Router.delete('/:book_id', controller.DeleteBook)
+Router.put('/:book_id', controller.UpdateBook)
 module.exports = Router
