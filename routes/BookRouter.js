3a81ab9dc9aa61a75ce606bc4a2ce6eb997b78b3
Daniel Shull <danielshull121@gmail.com>
2022-11-14 11:56:29 -0500

    delete back end route

--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -5,4 +5,5 @@ const controller = require('../controllers/BookController')
 Router.get('/all_books', controller.GetBooks)
 Router.get('/:book_id', controller.GetBookById)
 Router.post('/created_books', controller.CreateBook)
+Router.delete('/:book_id', controller.DeleteBook)
 module.exports = Router
