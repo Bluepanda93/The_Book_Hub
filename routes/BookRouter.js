fe83c7a72899cc5475ead811619f28b0df057e6e
Jerry Chen <jerry.chen33193@gmail.com>
2022-11-15 10:30:45 -0500

    nothing

--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -1,10 +1,9 @@
 const Router = require('express').Router()
 const controller = require('../controllers/BookController')
 
-
 Router.get('/all_books', controller.GetBooks)
 Router.get('/:book_id', controller.GetBookById)
-Router.post('/created_books', controller.CreateBook)
+Router.post('/', controller.CreateBook)
 Router.delete('/:book_id', controller.DeleteBook)
 Router.put('/:book_id', controller.UpdateBook)
 module.exports = Router
